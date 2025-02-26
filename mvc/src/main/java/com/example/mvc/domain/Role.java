019dad5f21b6cdceb3ff73c0f2ff15b66d0c118c
HUYEN <nguyenthithuhuyen29403@gmail.com>
2025-02-27 01:26:16 +0700

    update

--- mvc/src/main/java/com/example/mvc/domain/Role.java
+++ mvc/src/main/java/com/example/mvc/domain/Role.java
@@ -1,17 +1,25 @@
 package com.example.mvc.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "roles")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String description;
+
+    @OneToMany(mappedBy = "role")
+    private List<User> users;
 
     @Override
     public String toString() {
--- mvc/src/main/java/com/example/mvc/domain/User.java
+++ mvc/src/main/java/com/example/mvc/domain/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,6 +20,10 @@ public class User {
     private String address;
     private String phone;
     private String avatar;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
 
     @Override
     public String toString() {
